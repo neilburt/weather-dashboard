467fd8daf2981a185c3d02176d3109ed161a1581
neilburt <neil.burt@comcast.net>
2021-06-23 22:12:47 -0500

    Got .js to display desired current weather data from API on page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,34 @@
 const apiKey = "7ef239e2c4471a4b700261b5b01e28e3";
 const searchBtn = $('#search-button');
+const historyEl = $('#history');
+const weatherEl = $('#weather');
 
-function getApi() {
+function storeCity() {
+
+}
+
+function getHistory() {
+
+}
+
+function clearWeather() {
+  weatherEl.remove('div');
+}
+
+function getApiCurrent() {
   let searchEntry = $('#search-field').val();
-  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchEntry}&appid=${apiKey}`;
+  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchEntry}&units=imperial&appid=${apiKey}`;
 
   fetch(requestUrl)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data)
+      function renderCurrent () {
+        weatherEl.append(`<h4><strong>${data.name}</strong> ${moment().format('dddd, M-DD')}</h4>\n<p>Temp: ${Math.round(data.main.temp)}°</p>\n<p>Wind: ${Math.round(data.wind.speed)} MPH</p>\n<p>Humidity: ${data.main.humidity}%</p>\n<p>Feels Like: ${Math.round(data.main.feels_like)}°</p></div>`);
+      }
+      renderCurrent();
     })
-  console.log(searchEntry);
 }
 
-searchBtn.on('click', getApi);
+searchBtn.click(getApiCurrent);
